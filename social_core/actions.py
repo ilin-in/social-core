9d4106bf161357aec0d938af5f83028831451b78
Ilya <at.ilya@yahoo.com>
2017-06-22 15:16:10 +0300

    Update actions.py

--- social_core/actions.py
+++ social_core/actions.py
@@ -2,6 +2,10 @@ from six.moves.urllib_parse import quote
 
 from .utils import sanitize_redirect, user_is_authenticated, \
                    user_is_active, partial_pipeline_data, setting_url
+from rest_framework_jwt.serializers import jwt_payload_handler, jwt_encode_handler
+from rest_framework import status
+from django.http import HttpResponse
+import datetime
 
 
 def do_auth(backend, redirect_name='next'):
@@ -32,7 +36,7 @@ def do_complete(backend, login, user=None, redirect_name='next',
     data = backend.strategy.request_data()
 
     is_authenticated = user_is_authenticated(user)
-    user = user if is_authenticated else None
+    user = is_authenticated and user or None
 
     partial = partial_pipeline_data(backend, user, *args, **kwargs)
     if partial:
@@ -44,58 +48,65 @@ def do_complete(backend, login, user=None, redirect_name='next',
     # the pipeline so that the pipeline can change the redirect if needed
     redirect_value = backend.strategy.session_get(redirect_name, '') or \
                      data.get(redirect_name, '')
-
+    payload = "None"
     # check if the output value is something else than a user and just
     # return it to the client
     user_model = backend.strategy.storage.user.user_model()
     if user and not isinstance(user, user_model):
         return user
 
+    APP_URL_SCHEME_PREFIX = 'pickerUrl://?'
+    response = HttpResponse("", status=302)
     if is_authenticated:
         if not user:
-            url = setting_url(backend, redirect_value, 'LOGIN_REDIRECT_URL')
+            information = 'st=-1&m=User%20error'
         else:
-            url = setting_url(backend, redirect_value,
-                              'NEW_ASSOCIATION_REDIRECT_URL',
-                              'LOGIN_REDIRECT_URL')
+            payload = jwt_payload_handler(user)
+            sex = user.sex if user.sex else ''
+            bdate = ''
+            if user.birthday:
+                today = date.today()
+                if (today.year - user.birthday.year - ((today.month, today.day) < (user.birthday.month, user.birthday.day))) > 17:
+                    bdate = user.birthday.strftime('%d.%m.%Y')
+            req_info = '&b=' + bdate + '&s=' + str(sex)
+            information = 'st=1' + req_info
     elif user:
+        payload = jwt_payload_handler(user)
         if user_is_active(user):
             # catch is_new/social_user in case login() resets the instance
             is_new = getattr(user, 'is_new', False)
             social_user = user.social_user
-            login(backend, user, social_user)
+            # login(backend, user, social_user)
+
+
+            sex = user.sex if user.sex else ''
+            bdate = ''
+            if user.birthday:
+                today = date.today()
+                if (today.year - user.birthday.year - ((today.month, today.day) < (user.birthday.month, user.birthday.day))) > 17:
+                    bdate = user.birthday.strftime('%d.%m.%Y')
+            req_info = '&b=' + bdate + '&s=' + str(sex)
+
             # store last login backend name in session
-            backend.strategy.session_set('social_auth_last_login_backend',
-                                         social_user.provider)
+            backend.strategy.session_set('social_auth_last_login_backend', social_user.provider)
 
             if is_new:
-                url = setting_url(backend,
-                                  'NEW_USER_REDIRECT_URL',
-                                  redirect_value,
-                                  'LOGIN_REDIRECT_URL')
+                response['Location'] = APP_URL_SCHEME_PREFIX + 't=' + jwt_encode_handler(payload) + '&n=1&st=1' + req_info
+                return response
             else:
-                url = setting_url(backend, redirect_value,
-                                  'LOGIN_REDIRECT_URL')
+                response['Location'] = APP_URL_SCHEME_PREFIX + 't=' + jwt_encode_handler(payload) + '&n=-1&st=1' + req_info
+                return response
         else:
-            if backend.setting('INACTIVE_USER_LOGIN', False):
-                social_user = user.social_user
-                login(backend, user, social_user)
-            url = setting_url(backend, 'INACTIVE_USER_URL', 'LOGIN_ERROR_URL',
-                              'LOGIN_URL')
+            response['Location'] = APP_URL_SCHEME_PREFIX + 'st=-1&m=The%20user%20account%20is%20disabled'
+            return response
     else:
-        url = setting_url(backend, 'LOGIN_ERROR_URL', 'LOGIN_URL')
+        information = 'st=-1&m=Not%20found'
 
-    if redirect_value and redirect_value != url:
-        redirect_value = quote(redirect_value)
-        url += ('&' if '?' in url else '?') + \
-               '{0}={1}'.format(redirect_name, redirect_value)
-
-    if backend.setting('SANITIZE_REDIRECTS', True):
-        allowed_hosts = backend.setting('ALLOWED_REDIRECT_HOSTS', []) + \
-                        [backend.strategy.request_host()]
-        url = sanitize_redirect(allowed_hosts, url) or \
-              backend.setting('LOGIN_REDIRECT_URL')
-    return backend.strategy.redirect(url)
+    try:
+        response['Location'] = APP_URL_SCHEME_PREFIX + information + '&t=' + jwt_encode_handler(payload)
+    except TypeError:
+        response['Location'] = APP_URL_SCHEME_PREFIX + information
+    return response
 
 
 def do_disconnect(backend, user, association_id=None, redirect_name='next',
